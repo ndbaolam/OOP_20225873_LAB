822509a4b3534cabe69473d4f99178a44ef8395f
ndbaolam <baolam782004@gmail.com>
2024-12-02 18:52:50 +0700

    update: Cart class work with Media

--- Lab04/AimsProject/src/hust/soict/dsai/aims/cart/Cart.java
+++ Lab04/AimsProject/src/hust/soict/dsai/aims/cart/Cart.java
@@ -1,76 +1,47 @@
 package aims.cart;
 
-import aims.media.DigitalVideoDisc;
+import java.util.ArrayList;
+
+import aims.media.Media;
 
 public class Cart {
   public static final int MAX_NUMBERS_ORDERD = 20;
-  private DigitalVideoDisc itemsOrderd[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERD];
-  private int itemQuantity = 0;
+  private ArrayList<Media> itemsOrdered = new ArrayList<Media>();  
 
-  public void addDigitalVideoDisc(DigitalVideoDisc disc) {
-    if (itemQuantity == MAX_NUMBERS_ORDERD) {
-      System.out.println("The cart is almost full");
-      return;
-    }
-
-    itemsOrderd[itemQuantity++] = disc;
-    System.out.println("The disc has been added");
-  }
-
-  //Section 2: Working with method overloading
-  // public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
-  //   for(int i = 0; i <dvdList.length; i++) {
-  //     if (itemQuantity >= MAX_NUMBERS_ORDERD) { 
-  //       System.out.println("The cart is almost full");
-  //       break;
-  //     }
-  //     itemsOrderd[itemQuantity++] = dvdList[i];
-  //     System.out.println("The disc " + i + " has been added");
-  //   }
-  // }
-
-  public void addDigitalVideoDisc(DigitalVideoDisc... dvdList) {
-    for(DigitalVideoDisc disc : dvdList) {
-      if (itemQuantity >= MAX_NUMBERS_ORDERD) { 
+  public void addMedia(Media... mediaList) {
+    for(Media media : mediaList) {
+      if (itemsOrdered.size() >= MAX_NUMBERS_ORDERD) { 
         System.out.println("The cart is almost full");
         break;
       }
-      itemsOrderd[itemQuantity++] = disc;
-      System.out.println("The disc " + disc + " has been added");
+      itemsOrdered.add(media);
+      System.out.println("The disc " + media.getTitle() + " has been added");
     }
   }
 
-  public void addDigitalVideoDisc(DigitalVideoDisc dvd1,DigitalVideoDisc dvd2) {
-
-  }
-
-  //Section 6
   public void printCart() {
     System.out.println("***********************CART***********************");
-    System.out.println("Ordered Items:");
-    for (int i = 0; i < itemsOrderd.length; i++) {   
-      if(itemsOrderd[i] != null) {
-        System.out.println((i + 1) + ". DVD - " + itemsOrderd[i].toString());
+    System.out.println("Ordered Items:");    
+    for (int i = 0; i < itemsOrdered.size(); i++) {   
+      if(itemsOrdered.get(i) != null) {
+        System.out.println((i + 1) + ". DVD - " + itemsOrdered.get(i).toString());
       }        
     }
     System.out.println("Total cost: " + this.totalCost());
     System.out.println("**************************************************");
   }
 
-  public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
-    if (itemQuantity == 0) {
+  public void removeMedia(Media media) {
+    if (itemsOrdered.size() == 0) {
       System.out.println("The cart is almost empty");
       return;
     }
 
     boolean removed = false;
 
-    for (int i = 0; i < itemsOrderd.length; i++) {
-      if (itemsOrderd[i] != null && itemsOrderd[i].equals(disc)) {
-        for (int j = i; j < itemQuantity - 1; j++) {
-          itemsOrderd[j] = itemsOrderd[j + 1];
-        }
-        itemsOrderd[itemQuantity - 1] = null;
+    for (int i = 0; i < itemsOrdered.size(); i++) {
+      if (itemsOrdered.get(i) != null && itemsOrdered.get(i).equals(media)) {
+        itemsOrdered.remove(media);
         removed = true;
         break;
       }
@@ -81,15 +52,14 @@ public class Cart {
       return;
     }
 
-    itemQuantity--;
     System.out.println("The disc has been removed!");
   }
 
   public float totalCost() {
     float total = 0f;
-    for (int i = 0; i < itemsOrderd.length; i++) {   
-      if(itemsOrderd[i] != null)   
-        total += itemsOrderd[i].getCost();
+    for (int i = 0; i < itemsOrdered.size(); i++) {   
+      if(itemsOrdered.get(i) != null)   
+        total += itemsOrdered.get(i).getCost();
     }
     return total;
   }
